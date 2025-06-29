401f7c3f3326c0e3feed5d395f07cc2ab8fdaf88
gbatalov <georgy.batalov@gmail.com>
2025-06-30 01:23:25 +0500

    1.0.17.1

--- Formula/xtunnel.rb
+++ Formula/xtunnel.rb
@@ -5,10 +5,10 @@ class Xtunnel < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/xtunnel-dev/xtunnel-binaries/raw/refs/heads/main/1.0.17/xtunnel.osx-arm64.1.0.17.zip"
+      url "https://github.com/xtunnel-dev/xtunnel-binaries/raw/refs/heads/main/1.0.17/xtunnel.osx-arm64.1.0.17.1.zip"
       sha256 "02b1eef622c83f19d7c72c25fdbdeb6a5014bfa5cecf21e674a3e1b8cca9d101"
     else
-      url "https://github.com/xtunnel-dev/xtunnel-binaries/raw/refs/heads/main/1.0.17/xtunnel.osx-x64.1.0.17.zip"
+      url "https://github.com/xtunnel-dev/xtunnel-binaries/raw/refs/heads/main/1.0.17/xtunnel.osx-x64.1.0.17.1.zip"
       sha256 "be855d6a6d192628c6de1889ff1e52c3b8386801d133a5d1e1ae6263e70fdc58"
     end
   end
