055d9b820740d1152562c1aca5c1c268b033cc34
gbatalov <georgy.batalov@gmail.com>
2026-05-24 15:16:05 +0500

    chore: bump version to 2.6.0

--- Formula/xtunnel.rb
+++ Formula/xtunnel.rb
@@ -1,15 +1,15 @@
 class Xtunnel < Formula
   desc "Lightweight ngrok alternative tunnel utility"
   homepage "https://xtunnel.ru"
-  version "2.5.1"
+  version "2.6.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://dl.xtunnel.ru/v2.5.1/xtunnel-v2.5.1-osx-arm64.tar.gz"
-      sha256 "74508242eb77a2f20f129e533b44ae80bea99ef27739a130fbef78c7f4b0b193"
+      url "https://dl.xtunnel.ru/v2.6.0/xtunnel-v2.6.0-osx-arm64.tar.gz"
+      sha256 "212f28b3689b16b396e92c460555ebe4a506412f38487e7c1ac72387a7482506"
     else
-      url "https://dl.xtunnel.ru/v2.5.1/xtunnel-v2.5.1-osx-x64.tar.gz"
-      sha256 "3a2323320ff3d7ae2d3385461387e77c917cc71eeb2d81d5c3315dcf20019ae9"
+      url "https://dl.xtunnel.ru/v2.6.0/xtunnel-v2.6.0-osx-x64.tar.gz"
+      sha256 "37ce9781329d8150f01828b8dbdb61649e2c7486319b89b5a9698eab368fca53"
     end
   end
 
